f60b2d18856be6f275477d0d38bdec64f94f652c
LAPTOP-6B2P6FL8\Hari poorna <haripoornakumar@gmail.com>
2017-06-16 03:16:22 +0530

    bug fixes

--- scripts/basicContactForm.php
+++ scripts/basicContactForm.php
@@ -1,20 +1,21 @@
 <?php
     if (isset($_POST['submit'])) {
         $sname = $_POST['sname'];
-        $mobile_form = $_POST['mobile'];
+        /*$mobile_form = $_POST['mobile'];*/
+        $message = $_POST['message'];
         $email_form = $_POST['email'];     
-        $mobile = preg_match("/^[0-9\-]|[\+0-9]|[0-9\s]|[0-9()]*$/", $mobile_form)?$mobile_form:"invalid Mobile";
+        /*$mobile = preg_match("/^[0-9\-]|[\+0-9]|[0-9\s]|[0-9()]*$/", $mobile_form)?$mobile_form:"invalid Mobile";*/
         $email = preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/', $email_form)?$email_form:"invalid email";
 
         $to = "haripoornakumar@gmail.com";
 
         $subject = "Mail from Website contact Form";
 
-        $str = $sname . "  has contacted us on contact page of our website  whose mobile number is". $mobile . "   and email address is  " . $email;
+        /*$str = $sname . "  has contacted us on contact page of our website  whose mobile number is". $mobile . "   and email address is  " . $email;*/
         $txt = "Name of lead :  ". $sname."\r\n".
                 " Email of lead is : ". $email."\r\n".
-                 "Mobile number of lead is : ".$mobile;
-            @mail($to,$subject,$str,$txt);
+                 "Message  : ".$message;
+            @mail($to,$subject,$txt);
             /*header('location:http:../index.html');*/
             header( "refresh:2;url=../index.html" );
             echo "<h1>Thank you...! we contact you soon</h1>";
